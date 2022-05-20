4b33ff21eb2186151924605e284e5f50b6c1256a
Andrew <john@existtribe.com>
2022-05-20 11:50:07 +0800

    Added capacitor and ionic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import "../styles/ionic"
+
+import { defineCustomElements as ionDefineCustomElements } from '@ionic/core/loader';
+import { JSX as LocalJSX } from '@ionic/core'
+import { JSX as IoniconsJSX } from 'ionicons'
+import { HTMLAttributes, MutableRefObject, ReactText, useEffect } from 'react'
+
 import type { AppProps } from 'next/app'
 
+type ToReact<T> = {
+  [P in keyof T]?: T[P] & Omit<HTMLAttributes<Element>, 'className'> & {
+    class?: string;
+    key?: ReactText;
+    ref?: MutableRefObject<any>
+  }
+}
+
+declare global {
+  export namespace JSX {
+    interface IntrinsicElements extends ToReact<LocalJSX.IntrinsicElements & IoniconsJSX.IntrinsicElements> {}
+  }
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    ionDefineCustomElements(window)
+  }, [])
+
+  return (
+    <ion-app>
+      <Component {...pageProps} />
+    </ion-app>
+  )
 }
 
 export default MyApp
